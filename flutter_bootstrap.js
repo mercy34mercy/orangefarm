92d05712900f4f0de34de401db14740d8fb1660c
mercy34mercy <etukobamasatyan@gmail.com>
2025-08-24 10:49:21 +0900

    second

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"39d6d6e699e51b2874210e14cddf1a22fb9524
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "662918027"
+    serviceWorkerVersion: "562923742"
   }
 });
